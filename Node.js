f9f27e4f9136b58b458e49e3466b6bdca408035d
22maksaak-glitch <22maksa.ak@gmail.com>
2026-06-09 21:53:10 +0300

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,19 @@
+
+---
+
+### Код для Node.js (Socket.IO)
+
+**nodejs/package.json**
+```json
+{
+  "name": "chat-nodejs",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "socket.io": "^4.6.1"
+  }
+}
